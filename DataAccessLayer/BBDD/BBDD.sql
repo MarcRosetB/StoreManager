e953c8a6f68704485609100becbe03ae47365f78
Marc <m.roset.bermudez@gmail.com>
2023-12-20 23:36:07 +0100

    creada la tabla poblacion

--- DataAccessLayer/BBDD/BBDD.sql
+++ DataAccessLayer/BBDD/BBDD.sql
@@ -2,3 +2,8 @@ DROP DATABASE IF EXISTS StoreManager;
 CREATE DATABASE StoreManager;
 USE StoreManager;
 
+CREATE TABLE Poblacion (
+    IdPoblacion INT AUTO_INCREMENT PRIMARY KEY,
+    NombrePoblacion VARCHAR (30)
+);
+
