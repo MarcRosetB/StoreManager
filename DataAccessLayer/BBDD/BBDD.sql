0cc5e00a87931f808be2bd77ab74cb27e9e486b8
Marc <m.roset.bermudez@gmail.com>
2023-12-20 23:52:57 +0100

    creada la tabla empleado

--- DataAccessLayer/BBDD/BBDD.sql
+++ DataAccessLayer/BBDD/BBDD.sql
@@ -18,3 +18,14 @@ CREATE TABLE Departamento (
     IdDepartamento INT AUTO_INCREMENT PRIMARY KEY,
     NombreDepartamento VARCHAR (30)
 );
+
+CREATE TABLE Empleado (
+    IdEmpleado INT AUTO_INCREMENT PRIMARY KEY,
+    NombreEmpleado VARCHAR (50),
+    DNIEmpleado CHAR(9) UNIQUE,
+    EmailEmpleado VARCHAR(30) UNIQUE,
+    TelefonoEmpleado CHAR(9) UNIQUE,
+    Salario INT,
+    FkDepartamento INT,
+    FOREING KEY (FkDepartamento) REFERENCES Departamento(IdDepartamento)
+);
